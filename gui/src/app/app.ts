92ec6509ea512e2b7a951a4130ff3689c4445373
Marcos Diaz <diazmencia@gmail.com>
2025-08-03 15:27:09 +0300

    Basic board in angular

--- gui/src/app/app.ts
+++ gui/src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
 export class App {
   protected readonly title = signal('chess');
+  indexes = Array(64).fill(0).map((_, i) => 63 - i);
+
+  styleDarkCell = (index: number) => {
+    if (Math.floor(index / 8) % 2) {
+      if (!(index % 2)) return 'dark'
+    } else {
+      if (index % 2) return 'dark'
+    }
+    return ''
+  }
 }
